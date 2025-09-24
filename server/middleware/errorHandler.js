7d049c549451a417e482bb0805887ff8f5ac26d5
Atasha Frances Gayle Doria <asdoria@student.apc.edu.ph>
2025-09-24 06:27:25 +0000

    Redo everything

--- server/middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({
-    error: err.message || "Server Error",
-  });
-};
-
-module.exports = { errorHandler };
