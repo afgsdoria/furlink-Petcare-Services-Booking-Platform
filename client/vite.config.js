11231a2324cc25f4ac76697dac8efbde70bb9349
Atasha Frances Gayle Doria <asdoria@student.apc.edu.ph>
2025-10-08 18:34:54 +0000

    vite version

---
+++ client/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // 👈 required for GitHub Codespaces
+    port: 3000,
+    strictPort: true,
+    allowedHosts: true,
+    cors: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+})
